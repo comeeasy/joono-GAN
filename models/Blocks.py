791a42d9df41d865d3e95adc4de9d436c639a5d5
comeeasy <zz1236zz@naver.com>
2022-07-18 13:49:48 +0900

    add

--- models/Blocks.py
+++ models/Blocks.py
@@ -77,3 +77,103 @@ class TransposeConvBlock(nn.Module):
         out = self.layers(x)
 
         return out
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, downsample=False):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.btn_channels = in_channels // 2       # bottle neck channel
+        self.downsample = downsample
+
+        self.layer = nn.Sequential(
+            nn.Conv2d(self.in_channels, self.btn_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(self.out_channels // 2),
+            nn.ELU(inplace=True),
+            nn.Conv2d(self.btn_channels, self.out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(self.out_channels)
+        )
+
+        # bottle neck layer (1x1 Conv)
+        self.btn_layer = nn.Conv2d(self.in_channels, self.out_channels, kernel_size=1, padding=0)
+        # activation layer
+        self.atv_layer = nn.ELU(inplace=True)
+        self.pool = nn.MaxPool2d((2, 2))
+
+        self._init_weights(self.layer)
+        self._init_weights(self.btn_layer)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Conv2d):
+            nn.init.xavier_normal_(module.weight.data, 0.0, 0.02)
+            if module.bias is not None:
+                module.bias.data.zero_()
+        elif isinstance(module, nn.BatchNorm2d):
+            nn.init.xavier_normal_(module.weight.data, 1.0, 0.02)
+            nn.init.constant_(module.bias.data, 0)
+
+    def forward(self, x):
+        out = self.layer(x)
+
+        if self.in_channels != self.out_channels:
+            x = self.btn_layer(x)
+
+        out = x + out
+        out = self.atv_layer(out)
+
+        if self.downsample:
+            out = self.pool(out)
+
+        return out
+
+class ResTransposeBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, upsample=False):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.btn_channels = in_channels // 2
+        self.upsample = upsample
+        self.stride = 2 if upsample else 1
+        self.ksize = 4 if upsample else 3
+        self.pad = 1
+
+        self.layer = nn.Sequential(
+            nn.Conv2d(self.in_channels, self.btn_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(self.btn_channels),
+            nn.ELU(inplace=True),
+            nn.ConvTranspose2d(self.btn_channels, self.out_channels, kernel_size=self.ksize, stride=self.stride, padding=self.pad),
+            nn.BatchNorm2d(self.out_channels)
+        )
+        self.btn_layer = nn.ConvTranspose2d(self.in_channels, self.out_channels, kernel_size=self.ksize, stride=self.stride, padding=self.pad)
+        self.atv_layer = nn.ELU(inplace=True)
+
+        self._init_weights(self.layer)
+        self._init_weights(self.btn_layer)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Conv2d):
+            nn.init.xavier_normal_(module.weight.data, 0.0, 0.02)
+            if module.bias is not None:
+                module.bias.data.zero_()
+        if isinstance(module, nn.ConvTranspose2d):
+            nn.init.xavier_normal_(module.weight.data, 0.0, 0.02)
+            if module.bias is not None:
+                module.bias.data.zero_()
+        elif isinstance(module, nn.BatchNorm2d):
+            nn.init.xavier_normal_(module.weight.data, 1.0, 0.02)
+            nn.init.constant_(module.bias.data, 0)
+
+    def forward(self, x):
+        out = self.layer(x)
+
+        if self.out_channels != self.out_channels:
+            x = self.btn_layer(x)
+
+        out = x + out
+        self.atv_layer(out)
+
+        return out
+    
+
